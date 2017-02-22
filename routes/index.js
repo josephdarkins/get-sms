2d0567ee7f55ec9abd9e0ff71cf79d6736919b04
JoeDarkins <joe@fonix.com>
2017-02-22 14:40:41 +0000

    first commit

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/data/', function(req, res, next){
+   //console.log(req.body);      // your JSON
+
+   json={'players':[
+            {'name':"Messi", 'goals':8},            
+            {'name':"Ronaldo", 'goals':22},
+            {'name':"Costa", 'goals':20},
+            {'name':"Neymar", 'goals':13},
+            {'name':"Arabi", 'goals':6},
+            {'name':"Bale", 'goals':3},
+            {'name':"Toquero", 'goals':0}]};
+
+    JSON.stringify(json);
+
+   res.render('data', { data: json, title: 'joes page' });
+})
+
+router.post('/data/', function(req, res, next){
+   //console.log(req.body);      // your JSON
+
+   var jsonBody = JSON.stringify(req.body);
+
+   console.log(jsonBody);
+   res.end("yes");
+})
+
+module.exports = router;
+
