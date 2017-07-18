ad381839cfc4863e019f250c803caa343cb24d67
Joe Darkins <joseph.darkins@gmail.com>
2017-07-18 22:30:25 +0100

    maybe now

--- routes/index.js
+++ routes/index.js
@@ -21,8 +21,8 @@ router.get('/data/', function(req, res, next){
 
 router.post('/data/', function(req, res, next){
 
-  console.log(request.body.Body);
-  console.log(request.body.From);  
+  console.log(req.body.Body);
+  console.log(req.body.From);  
   response.send("<Response><Message>" + req.body.Body + "</Message></Response>");
 
    var jsonBody = JSON.stringify(req.body);
